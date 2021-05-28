5429e5f7a382c1f1e7a8b815619fbd4023e588ee
unknown <btstar_cy@korea.ac.kr>
2021-05-28 11:29:48 +0900

    Template from Spring Initializr

---
+++ src/test/kotlin/org/kweb/kotspring/week4/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.kweb.kotspring.week4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
